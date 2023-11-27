535cc9b4f63a8bb0b2a027ed4505e9ce84eec161
Aanis Nuur <Aanisn10@gmail.com>
2023-11-27 15:32:41 +0000

    Completed MVP

---
+++ task_events/code/lab_jsevents_start_code/script.js
@@ -0,0 +1,34 @@
+const enter = document.querySelector("#enter");
+const list = document.querySelector("#list");
+const showButton = document.querySelector("#show-button");
+const dateOutput = document.querySelector("#date-output")
+// const newToDoList = document.querySelector("#new-todo");
+
+// const textOutput = document.querySelector("text-output");
+
+enter.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const textInput = document.querySelector("#new-todo");
+    const newListItem = document.createElement("li");
+    newListItem.innerText = textInput.value;
+    list.appendChild(newListItem); 
+
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    newListItem.appendChild(deleteButton);
+    deleteButton.addEventListener("click", () => {
+        deleteButton.parentElement.remove();
+    })
+
+    
+});
+
+showButton.addEventListener("click", (e) => {
+    dateOutput.innerText = Date()
+})
+
+
+
+
+
